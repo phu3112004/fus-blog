9582c1c486a4a1e27c8dbc3d9ec08168282998d2
phu3112004 <22521105@gm.uit.edu.vn>
2024-08-02 16:34:01 +0700

    delete cssfile

--- src/index.js
+++ src/index.js
@@ -4,18 +4,28 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars') 
 const app = express()
 const port = 3000  
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 //http logger
 app.use(morgan("combined"))
 
 ///template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources\\views'))
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
 
 
 app.get('/', (req, res) => {
   res.render('home')
 })
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
